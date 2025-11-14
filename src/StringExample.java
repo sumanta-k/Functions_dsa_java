7a5c9c551f8ceb308e924cc4d3f3829373f6c9eb
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-11-14 12:16:46 +0530

    Fixes #5: create a function which takes argument from user
    Closes #6: takes username from user and show it at the end

--- src/StringExample.java
+++ src/StringExample.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class StringExample {
     public static void main(String[] args){
-        String ans = greet();
-        System.out.println(ans);
+//        String ans = greet();
+//        System.out.println(ans);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        String userName = in.next();
+        String personalizedMsg = greeting(userName);
+
+        System.out.println(personalizedMsg);
     }
 
     static String greet(){
         String greeting = "how are you";
         return greeting;
+    }
+
+    static String greeting(String name){
+        String message = "hello " + name;
+
+        return message;
     }
 }
