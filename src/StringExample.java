8d6504e5346585f54147b496eb2c988934ce4ea0
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-11-14 11:22:03 +0530

    Fixes #3: A function which is returning string type

---
+++ src/StringExample.java
@@ -0,0 +1,11 @@
+public class StringExample {
+    public static void main(String[] args){
+        String ans = greet();
+        System.out.println(ans);
+    }
+
+    static String greet(){
+        String greeting = "how are you";
+        return greeting;
+    }
+}
