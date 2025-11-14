0832a388d361cc13fa1f0ae0d74e05286403e3b7
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-11-14 16:16:51 +0530

    solves #8: demonstrate , where value has changed of the referenced object

---
+++ src/ValueChange.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class ValueChange {
+    public static void main(String [] args){
+
+        int [] arr = { 11, 22 ,33 ,44 ,55};
+        changeArr(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void changeArr(int [] arr){
+       arr[0] = 99;
+    }
+}
